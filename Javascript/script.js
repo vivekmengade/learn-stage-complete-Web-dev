270b5553a150776aa3fffc2530d94c8052da5629
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-06-10 18:03:50 +0530

    starting to learn javascript
    basic introduction of node.js and javascript uploading

---
+++ Javascript/script.js
@@ -0,0 +1,8 @@
+
+// alert("you are learnig Javascript...")
+// Document.title("introduction to JS")
+console.log("hello console")
+console.log("today new chapter begins")
+// document.title("hello web page")
+var a = prompt("enter any number")
+comsole.log("the number you type" + a)
