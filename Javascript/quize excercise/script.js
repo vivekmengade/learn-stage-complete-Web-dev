ad4d814e6f6e123da8fbc7e92951f4cd4f8fbffd
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-06-14 23:11:12 +0530

    Add files via upload

---
+++ Javascript/quize excercise/script.js
@@ -0,0 +1,13 @@
+// find age between 10yrs and 20 yrs
+let age = 20;
+console.log("age",age)
+if (age <= 10) {
+    console.log("age is smaller than 10");
+}
+else if(age<20) {
+    console.log("age is smaller than 20");
+
+}
+else{
+    console.log("age is greater than 20")
+}
