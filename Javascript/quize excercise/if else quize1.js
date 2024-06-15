eb3bf15d98cfeaf329041af8900c6e937cefb7c3
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-06-15 17:27:40 +0530

    Create if else quize1.js
    solving simple problem using conditions

---
+++ Javascript/quize excercise/if else quize1.js
@@ -0,0 +1,8 @@
+// find the number is divisible by 2/3
+let n = 3;
+if ((n % 3) == 0 && (n % 2) == 0 ) {
+    console.log("number is divisible by both 2&3")
+}
+else{
+    console.log("number is not divisible by both 2&3")
+}
