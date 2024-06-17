7b79a62a0e48aa0c3b887729d5130ff287e71e13
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-06-17 18:06:49 +0530

    Create mean use func.js
    find mean using function

---
+++ Javascript/quize excercise/mean use func.js
@@ -0,0 +1,7 @@
+// write code for find mean of 5 numbers
+function mean(a,b,c,d,e){
+    let ans= a+b+c+d+e
+    return ans/5
+}
+let result=mean(11,22,33,44,55);
+console.log("mean of numbers is",result)
