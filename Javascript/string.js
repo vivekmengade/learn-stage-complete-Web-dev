00cae1c481c72a84f1ae3ba804db8a75a1e43217
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-06-19 20:50:15 +0530

    Create string.js
    learn strings in js required functions

---
+++ Javascript/string.js
@@ -0,0 +1,19 @@
+// strings
+let a = "    Vivek Mengade";
+console.log(a);
+let nickname = "Bhaii";
+let con = a.concat(nickname)
+console.log(con)
+console.log(`hey i am ${a} and friends call me ${nickname}`)
+let caps = a.toUpperCase()
+let low=a.toLowerCase()
+console.log(caps)
+console.log(low)
+console.log(`length of a = ${a.length}`)
+console.log(`3,5 slice of a = ${a.slice(3,5)}`)
+console.log(`index=1 slice of a = ${a.slice(1)}`)
+let b=a.replace("V","j")
+console.log(`replace v=j of a = ${b}`)
+let conc=a.concat(${nickname});
+console.log(`hello ${conc} concated strings a & nickname`)
+console.log(a.trim())
