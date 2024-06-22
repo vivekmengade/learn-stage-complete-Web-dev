e57f27ba006e14a5fb32523859610788ef64dc49
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-06-23 00:00:35 +0530

    Create array.js
    learning array in js and some methods

---
+++ Javascript/array.js
@@ -0,0 +1,21 @@
+let arr=[1,2,3,4,5]
+console.log(arr.length)
+console.log(arr[4]);
+console.log(arr[0]);
+arr[0]=5;
+console.log(arr[0]);
+console.log(arr)
+console.log(arr.toString())
+console.log(typeof arr )
+console.log(arr.join(" sand "));
+console.log(arr.pop())
+console.log(arr)
+console.log(arr.push(5))
+console.log(arr)
+console.log(arr.shift())
+console.log(arr)
+console.log(arr.unshift(5))
+console.log(arr)
+console.log(delete arr[3])
+console.log(arr)
+
