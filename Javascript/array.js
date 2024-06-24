7daae362da20220412e9312c11658ea75d0106b8
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-06-24 22:15:59 +0530

    Update array.js
    new functions of array learning for mostly use in js

--- Javascript/array.js
+++ Javascript/array.js
@@ -19,3 +19,36 @@ console.log(arr)
 console.log(delete arr[3])
 console.log(arr)
 
+let arr = [1, 2, 3,8,9,20];
+let ar3 = [7, 8, 9];
+let ar2 = [4, 5, 6];
+let ar4 = [0];
+console.log(arr.concat(ar2, ar3, ar4));
+let con = arr.concat(ar3, ar2, ar4);
+con.sort()
+console.log(con)
+let sq=[];
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    sq.push(element*2)
+}
+console.log(arr)
+console.log(sq)
+let sqa=arr.map((e)=>{
+    return e+5
+})
+console.log(sqa)
+const gt7=(e)=>{
+    if(e>7){
+        return true
+    }
+    return false
+}
+console.log(arr.filter(gt7));
+const red=(a,b)=>{
+    // return a+b;
+    return a*b;
+}
+console.log(arr.reduce(red));
+let name=array.from("VIVEK")
+console.log(name)
