015395d6af9b9f54b7fedb2661d0253182e9ffae
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-07-25 20:53:38 +0530

    Create revision.js
    revision after log break step by step practicing all js

---
+++ Javascript/revision.js
@@ -0,0 +1,14 @@
+/ const a=5
+var a=6
+
+// let a=7     //wrong error of identifier already declared
+console.log("value of a is",a)
+// quick quize create a object which stores name, mobile no. , marks of students using object 
+const data={
+    name:"Vivek Mengade",
+    cont_no:8788658339,
+    marks:73.00
+}
+console.log(data)
+data.salary=1000;       //add keys in object
+console.log(data)
