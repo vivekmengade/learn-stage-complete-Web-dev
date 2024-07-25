7496e2d189d3b42e3c7df36a31634deeefbb64c6
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2024-07-25 20:55:50 +0530

    Update revision.js
    adding arithematic operators  section

--- Javascript/revision.js
+++ Javascript/revision.js
@@ -12,3 +12,15 @@ const data={
 console.log(data)
 data.salary=1000;       //add keys in object
 console.log(data)
+
+// arithematic operations 
+var a=5
+var b=3
+let add = a+b       
+let sub = a-b
+let div = a/b
+let mul = a*b
+console.log("add",add)
+console.log("sub",sub)
+console.log("div",div)
+console.log("mul",mul)
