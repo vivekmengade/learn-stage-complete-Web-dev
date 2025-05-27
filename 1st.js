361a210b92d49f6cc5b41d8e57e0205a8b64670b
vivekmengade <vivekmengade@gmail.com>
2025-05-27 20:43:12 +0530

    Initial commit for Day-1 JavaScript code

---
+++ 1st.js
@@ -0,0 +1,52 @@
+// console.log("1st.js");
+// alert("1st.js loaded");
+// console.log("Welcome Vivek!");
+// console.log("I love JS!");
+// name = "Vivek Mengade";
+// console.log("Name is: " + name);
+// age=21;
+// console.log("Age is: " + age);
+// price=20.50;
+// console.log("Price is: " + price);
+// x=null;
+// y=undefined;
+// console.log("x is: " + x);
+// console.log("y is: " + y);
+// isFollow=true;
+// console.log("isFollow :" + isFollow);
+// let a;
+// a=20;
+// let n=null;
+// typeof n;
+
+// let bigInt=BigInt(1234567890123456789012345678901234567890);
+// console.log("BigInt is: " + bigInt);
+
+// let sym =Symbol("Hello!");
+// console.log("Symbol is: " + y);
+
+
+const stu={
+    fullName: "Vivek Mengade",
+    age: 21,
+    cgpa:78.0,
+    isPassed: true
+}
+const product={
+    Name:"Pen Drive",
+    rating: 4.5,
+    offer: -75,
+    price:250.50
+};
+
+const id={
+    username: "vivek_mengade",
+    name: "Vivek Mengade",
+    post:125,
+    followers: 1000,
+    following: 500,
+    threadId:"@vivek_mengade",
+    Bio: "Web Developer, Open Source Contributor, and Tech Enthusiast",
+    isFollow: true,
+    acType: "Personal"
+}
