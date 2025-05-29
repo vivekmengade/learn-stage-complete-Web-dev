b7b7d83da4bdc7fabc958798ebf1fae91738a675
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2025-05-29 16:54:08 +0530

    Create 3rd.js
    day 3

---
+++ 3rd.js
@@ -0,0 +1,59 @@
+// let sum=0;
+// for(i=1; i<=5;i++){
+//     console.log(sum+= i);
+// }
+
+// let a= 0;
+// while(a<=0){
+//     console.log(a);
+//     a++;
+// }
+
+/*
+// Using do...while loop
+do{
+    console.log(a);
+    a++;
+}while(a<=5){
+    console.log("This will run at least once");
+    a++;
+}
+
+
+let str = "Vivek";
+let size=0;
+// Using for...of loop to iterate over the string
+for(let i of str){
+    console.log("i = ",i);
+    size++;
+}
+console.log("Size of the string is: ", size);
+
+
+
+// Using for...in loop to iterate over the string
+let student={
+    name:"Vivek",
+    age:20,
+    City:"Pune"
+}
+for(let i in student){
+    console.log("key= ",i,", Value = ", student[i]);
+}
+
+
+let a=100;
+for(let i=0;i<a; i++){
+    if(i%2==0){
+        console.log("Even number: ", i);
+    }
+}
+    
+let gno=10;
+let pno=prompt("enter a number to compare");
+while(gno!=pno){
+    console.log("correct number is: ", gno);
+    pno=prompt("guess next number");
+}
+    */
+   
