e9fe9da5553f1c48b1175987870120c322ff4fff
Vivek Mengade <141516691+vivekmengade@users.noreply.github.com>
2025-06-11 14:18:59 +0530

    Create 6th.js
    added DOM topic

---
+++ 6th.js
@@ -0,0 +1,12 @@
+// console.log("hello");
+// window.alert("hello world");
+// console.log(window);
+// window.console.log("hello world");
+// console.log(window.document);
+// console.log(document.body);
+
+console.dir(document.body.childNodes[0]);
+
+document.body.style.backgroundColor = "lightblue";
+document.body.style.color = "darkblue";
+document.body.children[3].innerText = "Hello, World!";
